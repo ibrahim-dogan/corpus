1b1e84b40360798989d9fa1f97c90973e0a03d32
Abraham <ibrahimmdogann@gmail.com>
2019-05-05 01:21:40 +0300

    :fire: json response done

---
+++ api/bapCleanAndTokenize.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 12:26:40 2019
+
+@author: ogulcan
+"""
+import re
+import json
+import nltk
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+
+
+
+
+def cleanAndTokenize(fil):
+
+    # with open('..\media\\'+filename) as fil:
+    data = json.load(fil)
+
+    # nltk.download('stopwords')
+    # nltk.download('punkt')
+
+    '''
+    data will come as json from FE with colnames(fileupload)
+    
+    -content
+    -date
+    -docid
+    -source
+    -title
+    
+    and create columns called tokens and tok 
+    
+    tokens in relative tokens tok is whole corpus
+    
+    tok column is deleted after operation
+    
+    and return frequency distiribution of tokens of whole corpus
+    
+    '''
+    p = re.compile(r'nonascii|.com|^https?:\/\/.*[\r\n]*|<.*?>|[^a-zA-Z ]+')
+    stop_words = stopwords.words("english")
+
+    data["tokens"] = {}
+
+    for docs in data["content"]:
+        data["content"][docs] = str(data["content"][docs]).lower()
+        data["content"][docs] = p.sub('', str(data["content"][docs]))
+        data["content"][docs] = " ".join(data["content"][docs].split())
+        data["tokens"][docs] = nltk.word_tokenize(str(data["content"][docs]))
+        data["tokens"][docs]=[word.lower() for word in data["tokens"][docs] if word.isalpha() if word not in stop_words]
+
+
+
+
+    data["tok"] = nltk.word_tokenize(str(data["content"]))
+
+    data["tok"]=[word.lower() for word in data["tok"] if word.isalpha() if word not in stop_words]
+    dist = FreqDist(data["tok"])
+    del data["tok"]
+    js = dist.most_common(10)#10 is arbitrary I could send whole data and it can be deduced in FE
+    # js =json.dumps(js)
+    return js
+
+
+# print(cleanAndTokenize("UK_afterJaccard.json"))
+
--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import FileSerializer
+from .bapCleanAndTokenize import cleanAndTokenize
+from django.http import JsonResponse
 
 
 class FileView(APIView):
@@ -12,14 +14,10 @@ class FileView(APIView):
         file_serializer = FileSerializer(data=request.data)
         if file_serializer.is_valid():
             file_serializer.save()
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            filename = str(request.data['file'])
+            data = cleanAndTokenize(request.data['file'])
+            # return Response(file_serializer.data, status=status.HTTP_201_CREATED)
 
-    def get(self, request, *args, **kwargs):
-        file_serializer = FileSerializer(data=request.data)
-        if file_serializer.is_valid():
-            file_serializer.save()
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+            return Response(data, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
