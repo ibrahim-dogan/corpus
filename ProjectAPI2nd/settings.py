928ca2153b0affd836ef9ff0b6bcec8c85783459
Abraham <ibrahimmdogann@gmail.com>
2019-05-05 21:55:11 +0300

    django-heroku

--- ProjectAPI2nd/settings.py
+++ ProjectAPI2nd/settings.py
@@ -122,17 +122,17 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-CORS_ORIGIN_WHITELIST = (
-    'google.com',
-    'hostname.example.com',
-    'localhost:8000',
-    '127.0.0.1:9000',
-    '127.0.0.1:8080',
-    'localhost:8080',
-    'corpus.tugberkgoc.now.sh'
-    'https://corpus.tugberkgoc.now.sh'
-
-)
+# CORS_ORIGIN_WHITELIST = (
+#     'google.com',
+#     'hostname.example.com',
+#     'localhost:8000',
+#     '127.0.0.1:9000',
+#     '127.0.0.1:8080',
+#     'localhost:8080',
+#     'corpus.tugberkgoc.now.sh'
+#     'https://corpus.tugberkgoc.now.sh'
+#
+# )
 
 CORS_ALLOW_HEADERS = (
     'accept',
@@ -146,5 +146,10 @@ CORS_ALLOW_HEADERS = (
     'x-requested-with',
     'cache-control',
 )
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 django_heroku.settings(locals())
