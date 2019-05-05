2b5f021970d2ddf8435bd57dd245ca58e77d34e9
Abraham <ibrahimmdogann@gmail.com>
2019-05-05 21:18:10 +0300

    django-heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ProjectAPI2nd.wsgi
---
+++ api/views.py
@@ -0,0 +1,23 @@
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import FileSerializer
+from .bapCleanAndTokenize import cleanAndTokenize
+from django.http import JsonResponse
+
+
+class FileView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        file_serializer = FileSerializer(data=request.data)
+        if file_serializer.is_valid():
+            file_serializer.save()
+            filename = str(request.data['file'])
+            data = cleanAndTokenize(request.data['file'])
+            # return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
