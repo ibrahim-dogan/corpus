98a48eb830e36f54bb27d5ddd54b555352011be2
dedinededin <ibrahimmdogann@gmail.com>
2019-06-02 20:28:40 +0300

    designing query endpoint

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import UploadFile, CleanWithParameters, Query
 
 urlpatterns = [
-    url('upload/', UploadFile.as_view(), name='file-upload'),
+    url('upload/', Query.as_view(), name='file-upload'),
     url('cleaning/', CleanWithParameters.as_view(), name='cleaning'),
     url('query/', Query.as_view(), name='test'),
 
