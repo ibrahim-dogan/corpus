85dba8d0b2b1bf64cdf434f9a75d8dfae480fe61
dedinededin <ibrahimmdogann@gmail.com>
2019-06-08 13:34:26 +0300

    delete unneccessary

--- corpus-backend/settings.py
+++ corpus-backend/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'api',
-    'urlarchive',
 ]
 
 MIDDLEWARE = [
--- urlarchive/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Category, URL
-
-# Register your models here.
-admin.site.register(URL)
-admin.site.register(Category)
