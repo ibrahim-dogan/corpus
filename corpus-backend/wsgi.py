1e5738f264795b11161072145c760085daa61a75
dedinededin <ibrahimmdogann@gmail.com>
2019-06-08 12:49:17 +0300

    rename project

--- corpus-backend/wsgi.py
+++ corpus-backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectAPI2nd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'corpus-backend.settings')
 
 application = get_wsgi_application()
